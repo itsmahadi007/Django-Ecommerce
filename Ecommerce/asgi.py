9f17858af036b4d14af219ce9abd04d6b0e3e9ce
Mahadi Hassan <itsmahadi@gmail.com>
2021-05-06 15:43:18 +0600

    initiallinzing startup files

--- Ecommerce/asgi.py
+++ Ecommerce/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectX project.
+ASGI config for Ecommerce project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectX.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
 
 application = get_asgi_application()
